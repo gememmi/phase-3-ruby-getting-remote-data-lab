cb4dc195d6dd17240829d03b1437660a33bb2d98
Emily <emelizadaniels@gmail.com>
2022-12-03 14:23:29 -0500

    commit tree 8b21e9f2caead17637e79f8393d94bf8e455000a

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,25 @@
-# Write your code here
+
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+   
+
+    # INITIALIZE EACH INSTANCE WITH A URL STRING
+    def initialize(url)
+        @url = url
+    end
+    # SENDS A GET REQUEST TO THE URL PASSED, RETURNS THE BODY OF THE RESPONSE
+def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+
+end
+
+def parse_json
+    JSON.parse(get_response_body)
+end
+
+end
